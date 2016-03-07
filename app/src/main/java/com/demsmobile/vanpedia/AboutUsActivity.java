d418c816964aa073270b35ab58fea102a71c2a35
Michelle Salema <Michelle Salema>
2016-03-07 10:21:20 -0800

    Added a "About us" activity via the side panel & corrected App name in Manifest (was "Welcome" now reverted back to "Vanpedia")

---
+++ app/src/main/java/com/demsmobile/vanpedia/AboutUsActivity.java
@@ -0,0 +1,20 @@
+package com.demsmobile.vanpedia;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+
+public class AboutUsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+
+}

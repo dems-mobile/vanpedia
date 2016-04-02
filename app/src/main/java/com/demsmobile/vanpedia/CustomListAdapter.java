3a16087263ccb2988e0210ac302fbccf6d383aa9
Eugene Vilder <vilder.eugene@gmail.com>
2016-04-01 22:33:08 -0700

    Add rating icons

--- app/src/main/java/com/demsmobile/vanpedia/CustomListAdapter.java
+++ app/src/main/java/com/demsmobile/vanpedia/CustomListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.demsmobile.vanpedia.places.Place;
@@ -47,7 +48,11 @@ public class CustomListAdapter extends BaseAdapter {
             holder = new ViewHolder();
             holder.nameView = (TextView) convertView.findViewById(R.id.title);
             holder.addressView = (TextView) convertView.findViewById(R.id.address);
-            holder.ratingView = (TextView) convertView.findViewById(R.id.rating);
+            holder.ratingImage1 = (ImageView) convertView.findViewById(R.id.ratingImage1);
+            holder.ratingImage2 = (ImageView) convertView.findViewById(R.id.ratingImage2);
+            holder.ratingImage3 = (ImageView) convertView.findViewById(R.id.ratingImage3);
+            holder.ratingImage4 = (ImageView) convertView.findViewById(R.id.ratingImage4);
+            holder.ratingImage5 = (ImageView) convertView.findViewById(R.id.ratingImage5);
             convertView.setTag(holder);
 
         } else {
@@ -55,8 +60,43 @@ public class CustomListAdapter extends BaseAdapter {
         }
         holder.nameView.setText(listData.get(position).name);
         holder.addressView.setText(listData.get(position).vicinity);
-       // holder.ratingView.setText(listData.get(position).rating);
-        holder.ratingView.setText(listData.get(position).rating + "");
+
+        int ratingImageNum = (int)Math.ceil(listData.get(position).rating);
+
+        holder.ratingImage1.setImageResource(R.drawable.filled_star_gray_dark);
+        holder.ratingImage2.setImageResource(R.drawable.filled_star_gray_dark);
+        holder.ratingImage3.setImageResource(R.drawable.filled_star_gray_dark);
+        holder.ratingImage4.setImageResource(R.drawable.filled_star_gray_dark);
+        holder.ratingImage5.setImageResource(R.drawable.filled_star_gray_dark);
+
+        switch (ratingImageNum){
+            case 1:
+                holder.ratingImage1.setImageResource(R.drawable.filled_star);
+                break;
+            case 2:
+                holder.ratingImage1.setImageResource(R.drawable.filled_star);
+                holder.ratingImage2.setImageResource(R.drawable.filled_star);
+                break;
+            case 3:
+                holder.ratingImage1.setImageResource(R.drawable.filled_star);
+                holder.ratingImage2.setImageResource(R.drawable.filled_star);
+                holder.ratingImage3.setImageResource(R.drawable.filled_star);
+                break;
+            case 4:
+                holder.ratingImage1.setImageResource(R.drawable.filled_star);
+                holder.ratingImage2.setImageResource(R.drawable.filled_star);
+                holder.ratingImage3.setImageResource(R.drawable.filled_star);
+                holder.ratingImage4.setImageResource(R.drawable.filled_star);
+                break;
+            case 5:
+                holder.ratingImage1.setImageResource(R.drawable.filled_star);
+                holder.ratingImage2.setImageResource(R.drawable.filled_star);
+                holder.ratingImage3.setImageResource(R.drawable.filled_star);
+                holder.ratingImage4.setImageResource(R.drawable.filled_star);
+                holder.ratingImage5.setImageResource(R.drawable.filled_star);
+                break;
+        }
+        //holder.ratingImage1.setImageResource(R.drawable.outline_star);//.setText(listData.get(position).rating + "");
 
         return convertView;
     }
@@ -64,7 +104,11 @@ public class CustomListAdapter extends BaseAdapter {
     static class ViewHolder {
         TextView nameView;
         TextView addressView;
-        TextView ratingView;
+        ImageView ratingImage1;
+        ImageView ratingImage2;
+        ImageView ratingImage3;
+        ImageView ratingImage4;
+        ImageView ratingImage5;
     }
 }
 
